63bb80405bd00c3226367d31704a2907ecbcf15d
Joey Ballentine <joeyjballentine@gmail.com>
2022-04-19 21:41:06 -0400

    remove sprintfs in data reader

--- src/datareader.cpp
+++ src/datareader.cpp
@@ -123,7 +123,7 @@ int DataReaderFromMemory::scan(const char* format, void* p) const
     size_t fmtlen = strlen(format);
 
     char* format_with_n = new char[fmtlen + 4];
-    sprintf(format_with_n, "%s%%n", format);
+    // sprintf(format_with_n, "%s%%n", format);
 
     int nconsumed = 0;
     int nscan = sscanf((const char*)d->mem, format_with_n, p, &nconsumed);
@@ -195,7 +195,7 @@ int DataReaderFromAndroidAsset::scan(const char* format, void* p) const
     int fmtlen = strlen(format);
 
     char* format_with_n = new char[fmtlen + 3];
-    sprintf(format_with_n, "%s%%n", format);
+    // sprintf(format_with_n, "%s%%n", format);
 
     int nconsumed = 0;
     int nscan = sscanf((const char*)d->mem, format_with_n, p, &nconsumed);
