2914d4946cca32a2685ffef7bdd0c7718c5e6d44
Joey Ballentine <joeyjballentine@gmail.com>
2022-08-26 10:22:22 -0400

    please

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ class CMakeBuild(build_ext):
         # subprocess.check_call(
         #     ["cmake", "--build", "."] + build_args, cwd=self.build_temp
         # )
-        subprocess.check_call(["cp", "-a", "/build/.", self.build_temp])
+        subprocess.check_call(["cp", "-a", "build", self.build_temp])
 
 
 if sys.version_info < (3, 0):
